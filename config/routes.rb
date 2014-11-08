95bea9b8dce0d990f57e4422b9d470a86b6d2cf5
Tate Price <priceat@gmail.com>
2014-11-08 15:32:08 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Blocitoff::Application.routes.draw do
-  root "application#hello"
-  
+
   devise_for :users
-  get "welcome/index"
-  get "welcome/about"
+  
   resources :todos, only: [:new]
 
+  get 'about' => 'welcome#about'
+  
   root to: 'welcome#index'
 
 end
